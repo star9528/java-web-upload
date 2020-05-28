bc4d00bf910fb97385a30aaf496fdbef76fb8fbd
飞 <1752840757@qq.com>
2020-05-28 22:17:46 +0800

    线程池

---
+++ Test19/src/MyThreadPool.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.BlockingDeque;
+
+public class MyThreadPool {
+    private BlockingDeque<Runnable> queue;
+
+    public MyThreadPool(int) {
+        this.queue = queue;
+    }
+}
